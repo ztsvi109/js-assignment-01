685e7c64aa4ecae5f5202850d06e918ce6b3e29d
sarthak verma <sarthak.verma.int@zehntech.com>
2020-02-10 16:10:49 +0530

    first js assignment

---
+++ Java Script Project 1/CAPSTONE.js
@@ -0,0 +1,64 @@
+function changeImg() {
+    var image = document.getElementById('wstar');
+    console.log(image.src);
+    if (image.src.match("w_star")) {
+        image.src = "y_star.png";
+    } else {
+        image.src = "w_star.png";
+    }
+}
+
+
+function changeImg1() {
+    var image = document.getElementById('wstar1');
+    console.log(image.src);
+    if (image.src.match("w_star")) {
+        image.src = "y_star.png";
+    } else {
+        image.src = "w_star.png";
+    }
+}
+
+function changeImg2() {
+    var image = document.getElementById('wstar2');
+    console.log(image.src);
+    if (image.src.match("w_star")) {
+        image.src = "y_star.png";
+    } else {
+        image.src = "w_star.png";
+    }
+}
+
+
+
+function changeImg3() {
+    var image = document.getElementById('wstar3');
+    console.log(image.src);
+    if (image.src.match("w_star")) {
+        image.src = "y_star.png";
+    } else {
+        image.src = "w_star.png";
+    }
+}
+
+
+function changeImg4() {
+    var image = document.getElementById('wstar4');
+    console.log(image.src);
+    if (image.src.match("w_star")) {
+        image.src = "y_star.png";
+    } else {
+        image.src = "w_star.png";
+    }
+}
+
+
+function changeImg5() {
+    var image = document.getElementById('wstar5');
+    console.log(image.src);
+    if (image.src.match("w_star")) {
+        image.src = "y_star.png";
+    } else {
+        image.src = "w_star.png";
+    }
+}
